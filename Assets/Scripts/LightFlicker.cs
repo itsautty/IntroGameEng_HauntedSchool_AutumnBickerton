cefbc8075b3f18d7e2fda8c7ada29e915b2ff941
Autumn Bickerton <a.xrkour@gmail.com>
2026-01-04 00:22:42 -0400

    AddedFlickeringLightsAndLockers

---
+++ Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class FlickeringLight : MonoBehaviour
+{
+    private Light light;
+
+    public float minIntensity = .5f;
+    public float maxIntensity = 5.0f;
+    public float flickerSpeed = 0.1f;
+
+    private void Start()
+    {
+        light = GetComponent<Light>();
+
+        InvokeRepeating("Flicker", 0f, flickerSpeed);
+    }
+
+    private void Flicker()
+    {
+        float randomIntensity = Random.Range(minIntensity, maxIntensity);
+        light.intensity = randomIntensity;
+    }
+}
